4faded7e9a9454078ba9894fcaa8b8fcf6801791
soheekimdev <soheekim.dev@gmail.com>
2025-01-03 16:39:15 +0900

    feat: InputWithLabel: props에 className 추가

--- src/components/form/InputWithLabel.tsx
+++ src/components/form/InputWithLabel.tsx
@@ -1,14 +1,24 @@
+import clsx from 'clsx';
+
 type Direction = 'row' | 'col';
 
 interface InputWithLabelProps {
   children: React.ReactNode;
   direction?: Direction;
+  className?: string;
 }
 
-export default function InputWithLabel({ children, direction = 'col' }: InputWithLabelProps) {
+export default function InputWithLabel({
+  children,
+  direction = 'col',
+  className,
+}: InputWithLabelProps) {
   return (
     <div
-      className={`flex ${direction === 'row' ? 'flex-row items-center justify-between' : 'flex-col'} gap-2`}
+      className={clsx(
+        `flex ${direction === 'row' ? 'flex-row items-center justify-between' : 'flex-col'} gap-2`,
+        className,
+      )}
     >
       {children}
     </div>
