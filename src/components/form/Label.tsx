633a20c84460fca12e715e33ca5892353295d42a
soheekimdev <soheekim.dev@gmail.com>
2025-01-09 11:57:31 +0900

    refactor: Input, Label 컴포넌트에 error props 추가(optional)

--- src/components/form/Label.tsx
+++ src/components/form/Label.tsx
@@ -1,12 +1,14 @@
 interface LabelProps {
   text: string;
   htmlFor?: string;
+  required?: boolean;
 }
 
-export default function Label({ text, htmlFor }: LabelProps) {
+export default function Label({ text, htmlFor, required }: LabelProps) {
   return (
     <label htmlFor={htmlFor} className="text-sm font-medium text-13">
       {text}
+      {required && <span className="ml-0.5">*</span>}
     </label>
   );
 }
