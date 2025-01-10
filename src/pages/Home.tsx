8eb8ef7712bb5b9e89e29d0470a2cbae07b6394d
kosmosticlay <kosmosticlay@gmail.com>
2025-01-10 16:36:38 +0900

    feat: 로그인 된 user 상태 정보 연동 및 더미데이터 추가
    - 리덕스의 user 상태에서 access token 사용하도록 수정
    - API 연동 전 UI 테스트를 위해 컴포넌트 내 더미데이터 추가
    - 실제 API 호출 코드는 주석 처리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,23 +2,86 @@ import { useEffect, useState } from 'react';
 import { fetchHomeData } from '../api/home';
 import HomeCategory from '../components/home/HomeCategory';
 import { HomeUserData } from '../types/home/home.types';
-// import { useSelector } from 'react-redux';
-// import { RootState } from '../store';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+import { Tokens } from '../types/auth';
 
 export default function Home() {
-  // const { user } = useSelector((state: RootState) => state);
+  const { user } = useSelector((state: RootState) => state);
   const [homeData, setHomeData] = useState<HomeUserData>({ forms: [], asks: [] });
   const [isLoading, setIsLoading] = useState(true);
 
-  const USER_ID = 111;
+  const userId = (user as { tokens: Tokens }).tokens.access;
+
+  const testHomeData: HomeUserData = {
+    forms: [
+      {
+        author_id: 111,
+        id: 124,
+        title: '오즈_만족도조사_6기_14주',
+        tag: '6기만족도',
+        end_at: '20260116',
+        status: '발행',
+        target_count: 100,
+        actual_count: 56,
+      },
+      {
+        author_id: 111,
+        id: 125,
+        title: '오즈_만족도조사_6기_15주',
+        tag: '6기만족도',
+        end_at: '20250116',
+        status: '발행',
+        target_count: 100,
+        actual_count: 56,
+      },
+      {
+        author_id: 111,
+        id: 131,
+        title: '오즈_만족도조사_6기_16주',
+        tag: '6기만족도',
+        end_at: '20250116',
+        status: '종료',
+        target_count: 100,
+        actual_count: 94,
+      },
+    ],
+    asks: [
+      {
+        author_id: 111,
+        id: 211,
+        title: '첫 번째 애스크',
+        tag: '6기만족도',
+        is_closed: false,
+        access_code: 'ABC123',
+      },
+      {
+        author_id: 111,
+        id: 212,
+        title: '두 번째 애스크',
+        tag: '6기',
+        is_closed: false,
+        access_code: 'ABC123',
+      },
+      {
+        author_id: 111,
+        id: 222,
+        title: '세 번째 애스크',
+        tag: '6기만족도',
+        is_closed: true,
+        access_code: 'ABC123',
+      },
+    ],
+  };
 
   useEffect(() => {
     const loadHomeData = async () => {
       try {
         setIsLoading(true);
-        const data = await fetchHomeData(USER_ID);
-        console.log('API 응답 데이터:', data);
-        setHomeData(data);
+        // const data = await fetchHomeData(userId);
+        // console.log('API 응답 데이터:', data);
+        // setHomeData(data);
+        setHomeData(testHomeData);
       } catch (err) {
         console.error('데이터 로딩 중 에러:', err);
       } finally {
@@ -27,7 +90,7 @@ export default function Home() {
     };
 
     loadHomeData();
-  }, [USER_ID]);
+  }, [userId]);
 
   if (isLoading) return <div>로딩 중..로딩 중..로딩 중..로딩 중..로딩 중..로딩 중..로딩 중...</div>; // 로딩 컴포넌트 추가 예정
 
