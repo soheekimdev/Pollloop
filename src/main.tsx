a563cdb2b6be86027833466c17f05cfab71b12bc
soheekimdev <soheekim.dev@gmail.com>
2025-01-10 14:11:06 +0900

    chore: json-server 라이브러리 삭제 및 pnpm 재인스톨

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
 import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
