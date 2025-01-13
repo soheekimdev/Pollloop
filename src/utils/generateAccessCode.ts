b214998a36c82eead1dcc2d081940735d3ee31c9
soheekimdev <soheekim.dev@gmail.com>
2025-01-13 15:21:09 +0900

    feat: 랜덤 비밀번호 생성 로직 추가

---
+++ src/utils/generateAccessCode.ts
@@ -0,0 +1,12 @@
+export function generateAccessCode(): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const length = 8;
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+
+  return result;
+}
