730f673ff0b7224af2c97f22f39b76b947035bf2
hatnim <dlgotsla3391@gmail.com>
2025-01-15 23:13:46 +0900

    feat:비공개 참여폼 코드 입력 페이지 구현(임시)

--- src/layouts/FormLayout.tsx
+++ src/layouts/FormLayout.tsx
@@ -1,7 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 import Navbar from '../components/navbar/Navbar';
+import { useFormData } from '@/hooks/useFormData';
+import { errorToast, successToast } from '@/utils/toast';
+import FormPasswordCheck from '@/pages/forms/FormPasswordCheck';
 
 export default function FormLayout() {
+  const { formId } = useParams();
+  const { formData } = useFormData(formId || '');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handlePasswordSubmit = (accessCode: string) => {
+    if (accessCode === formData?.access_code) {
+      setIsAuthenticated(true);
+      successToast('인증되었습니다.');
+    } else {
+      errorToast('올바르지 않은 코드입니다.');
+    }
+  };
+
+  if (formData?.is_private && !isAuthenticated) {
+    return <FormPasswordCheck onSubmit={handlePasswordSubmit} />;
+  }
+
   return (
     <div className="flex flex-col h-full gap-4 overflow-auto bg-pollloop-bg-02">
       <Navbar />
